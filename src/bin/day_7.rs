e9ed14b8072d77621d56d50768ac181e9dd2ca65
C6OI <C6OI@yandex.ru>
2025-12-08 02:16:25 +0500

    Day 7 part 2
    fuck this day

--- src/bin/day_7.rs
+++ src/bin/day_7.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 const INPUT: &str = include_str!("../../inputs/day_7/input");
 
 fn main() {
@@ -15,26 +17,27 @@ fn part_1() -> i64 {
 
     for line in INPUT.lines() {
         for (idx, char) in line.chars().enumerate() {
-            if char == 'S' {
-                beam_positions = vec![None; width];
-                beam_positions[idx] = Some(());
-                break;
-            }
-
-            if char == '.' {
-                continue;
-            }
-
-            if char == '^' && beam_positions[idx].take().is_some() {
-                if idx > 0 {
-                    beam_positions[idx - 1] = Some(());
+            match char {
+                'S' => {
+                    beam_positions = vec![None; width];
+                    beam_positions[idx] = Some(());
+                    break;
                 }
+                '^' => {
+                    if beam_positions[idx].take().is_some() {
+                        if idx > 0 {
+                            beam_positions[idx - 1] = Some(());
+                        }
 
-                if idx < width - 1 {
-                    beam_positions[idx + 1] = Some(());
+                        if idx < width - 1 {
+                            beam_positions[idx + 1] = Some(());
+                        }
+
+                        res += 1;
+                    }
                 }
-
-                res += 1;
+                '.' => continue,
+                _ => panic!("Invalid symbol '{char}'"),
             }
         }
     }
@@ -44,6 +47,58 @@ fn part_1() -> i64 {
 
 fn part_2() -> i64 {
     let mut res = 0;
+    let mut beam_position = None;
+
+    let mut splits: HashMap<(usize, usize), i64> = HashMap::new();
+    let mut splitters: HashSet<(usize, usize)> = HashSet::new();
+
+    for (line_idx, line) in INPUT.lines().enumerate() {
+        for (char_idx, char) in line.chars().enumerate() {
+            match char {
+                'S' if beam_position.is_none() => {
+                    beam_position = Some(char_idx);
+                    break;
+                }
+                '^' => {
+                    if res == 0 && beam_position == Some(char_idx) {
+                        splits.insert((char_idx - 1, line_idx), 1);
+                        splits.insert((char_idx + 1, line_idx), 1);
+
+                        res += 2;
+                    }
+
+                    splitters.insert((char_idx, line_idx));
+                }
+                '.' => continue,
+                _ => panic!("Invalid symbol '{char}'"),
+            }
+        }
+    }
+
+    let lines_length = INPUT.lines().count();
+
+    while let Some(((beam_index, line_index), &count)) = splits.clone().iter().next() {
+        splits.remove(&(*beam_index, *line_index));
+
+        for line_index in (line_index + 1)..lines_length {
+            if !splitters.contains(&(*beam_index, line_index)) {
+                continue;
+            }
+
+            splits
+                .entry((beam_index - 1, line_index))
+                .and_modify(|left_count| *left_count += count)
+                .or_insert(count);
+
+            splits
+                .entry((beam_index + 1, line_index))
+                .and_modify(|right_count| *right_count += count)
+                .or_insert(count);
+
+            res += count;
+            break;
+        }
+    }
 
     res
 }
